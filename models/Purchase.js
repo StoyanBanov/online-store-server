9e49bcad43f76d795880fc0cdd6232cc6f1b8e54
StoyanBanov <sbanov05@gmail.com>
2023-08-16 16:17:27 +0300

    add purchase model

---
+++ models/Purchase.js
@@ -0,0 +1,29 @@
+const { Schema, model } = require('mongoose')
+
+const schema = new Schema({
+    items: [
+        {
+            item: {
+                type: ObjectId,
+                ref: 'Item',
+                required: true
+            },
+            count: {
+                type: Number,
+                required: true,
+                min: [0, 'Count can\'t be negative'],
+                validate: {
+                    validator: value => Number.isInteger(value),
+                    message: 'Count must be an integer'
+                }
+            },
+        }
+    ],
+    paymentMethod: { type: String, required: true, enum: ['cash', 'card'] },
+    deliverTo: { type: String, required: true, enum: ['office', 'address'] },
+    address: { type: String, required: true }
+})
+
+const Purchase = model('Purchase', schema)
+
+module.exports = Purchase
